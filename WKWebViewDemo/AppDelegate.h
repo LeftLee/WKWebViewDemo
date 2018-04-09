8db95a5616dd0bc78cc51e70a59a51609fd6b3a4
李展 <Azzan@lzpro.local>
2018-04-09 09:44:08 +0800

    Initial Commit

---
+++ WKWebViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebViewDemo
+//
+//  Created by Azzan on 2018/4/9.
+//  Copyright © 2018年 Azzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKWebViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebViewDemo
+//
+//  Created by Azzan on 2018/4/9.
+//  Copyright © 2018年 Azzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
